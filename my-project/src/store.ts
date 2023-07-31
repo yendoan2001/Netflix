5c87c02ce512c061b9512be80983fdf5ea8b0fee
Yen <yenjy8@gmail.com>
2023-07-31 23:28:26 +0700

    navbar

--- my-project/src/store.ts
+++ my-project/src/store.ts
@@ -1,15 +1,15 @@
-import {configureStore} from '@reduxjs/toolkit'
-import {setupListeners} from '@reduxjs/toolkit/query'
-import {pokemonApi} from './services/pokemon'
-
-export const store = configureStore({
-    reducer: {
-        [pokemonApi.reducerPath]: pokemonApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(pokemonApi.middleware),
-})
-
-setupListeners(store.dispatch)
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+// import {configureStore} from '@reduxjs/toolkit'
+// import {setupListeners} from '@reduxjs/toolkit/query'
+// import {pokemonApi} from './services/pokemon'
+//
+// export const store = configureStore({
+//     reducer: {
+//         [pokemonApi.reducerPath]: pokemonApi.reducer,
+//     },
+//     middleware: (getDefaultMiddleware) =>
+//         getDefaultMiddleware().concat(pokemonApi.middleware),
+// })
+//
+// setupListeners(store.dispatch)
+// export type RootState = ReturnType<typeof store.getState>
+// export type AppDispatch = typeof store.dispatch
